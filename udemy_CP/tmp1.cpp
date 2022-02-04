cb49cb28de07b4640b186fd76d6bcdaf87ff1d0b
anuj <anujkumar63166316@gmail.com>
2022-02-04 18:19:41 +0530

    Dir for CP practice code

---
+++ udemy_CP/tmp1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#define ll long long int
+using namespace std;
+
+int main(){
+
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+	#endif
+	ll n = 10e8; // 10e8==10^8
+	cout<<n<<endl;
+	int max=INT_MAX; // comes in bits/stdc++.h max value a int can hold;
+	int min=INT_MIN;
+	cout<<max<<endl;
+	cout<<min<<endl;
+
+}
