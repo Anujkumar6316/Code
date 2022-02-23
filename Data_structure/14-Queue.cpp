8e153a6d8fbd640cc6f6943052bf6ad4c6566f18
anuj kumar <anujkumar63166316@gmail.com>
2022-02-23 22:45:35 +0530

    update in queue file

--- Data_structure/14-Queue.cpp
+++ Data_structure/14-Queue.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 using namespace std;
 
+void std_file(){
+	#ifndef online_judge
+	freopen("/home/anuj/input.txt","r",stdin);
+	freopen("/home/anuj/output.txt","w",stdout);
+	#endif
+}
+
 
 //** QUEUE USING ARRAY **
 
@@ -59,7 +66,199 @@ void queue::display_quene(){
 	cout<<endl;
 }
 
+//** CIRCULAR QUEUE **
+
+class circular_queue{
+	int size;
+	int front;
+	int rear;
+	int *arr;
+public:
+	circular_queue(){
+		size=10;
+		front=0;
+		rear=0;
+		arr=new int[size];
+	}
+	circular_queue(int s){
+		size=s;
+		front=0;
+		rear=0;
+		arr=new int[size];
+	}
+	void enqueue(int ele);
+	int dequeue();
+	void display_quene();
+};
+
+void circular_queue::enqueue(int ele){
+	if(rear==0){
+		rear=(rear+1)%size;
+		arr[rear]=ele;
+	}
+	else if((rear+1)%size!=front){
+		rear=(rear+1)%size;
+		arr[rear]=ele;
+	}
+	else
+		cout<<"Circular queue is full"<<endl;
+}
+
+int circular_queue::dequeue(){
+	int x=-1;
+	if(front==rear)
+		cout<<"Circular queue is empty"<<endl;
+	else{
+		front=(front+1)%6;
+		x=arr[front];
+	}
+	return x;
+}
+
+void circular_queue::display_quene(){
+	int x=front;
+	while(x!=rear){
+		x=(x+1)%size;
+		cout<<arr[x]<<" ";
+	}
+	cout<<endl;
+}
+
+// ** QUEUE USING LINKED LIST **
+
+class node{
+public:
+	int data;
+	node *next;
+	// void enqueue(int ele); //this is also a right way to do it.
+	// int dequeue();
+	// void display_quene();
+}*front=NULL,*rear=NULL,*front_deq=0,*rear_deq=0;
+
+void enqueue(int ele){
+	node *p=new node();
+
+	if(p==0)
+		cout<<"Queue is full"<<endl;
+	else{
+		p->data=ele;
+		p->next=0;
+		if(front==0){
+			front=p;
+			rear=p;
+		}
+		else{
+			rear->next=p;
+			rear=p;
+		}
+	}
+}
+
+int dequeue(){
+	int x;
+	if(front==0)
+		cout<<"LL Queue is empty"<<endl;
+	else{
+		x=front->data;
+		front=front->next;
+	}
+	return x;
+}
+
+void display_quene(){
+	node *p=front;
+	while(p){
+		cout<<p->data<<" ";
+		p=p->next;
+	}
+	cout<<endl;
+}
+
+// ** DEqueue ** --> doubly ended queue
+
+void front_enqueue(int ele){
+	node *p=new node();
+	if(p==0)
+		cout<<"DEqueue is full"<<endl;
+	else{
+		p->data=ele;
+		p->next=front_deq;
+		if(front_deq==0){
+			front_deq=p;
+			rear_deq=p;
+		}
+		else
+			front_deq=p;
+	}
+}
+void rear_enqueue(int ele){
+	node *p=new node();
+	if(p==0)
+		cout<<"DEqueue is full"<<endl;
+	else{
+		p->data=ele;
+		p->next=0;
+		if(front_deq==0){
+			front_deq=p;
+			rear_deq=p;
+		}
+		else{
+			rear_deq->next=p;
+			rear_deq=p;
+		}
+	}
+}
+
+//landra 
+
+int front_dequeue(){
+	int x=-1;
+	if(front_deq==0)
+		cout<<"DEqueue is empty"<<endl;
+	else{
+		x=front_deq->data;
+		front_deq=front_deq->next;
+	}
+	return x;
+}
+
+//this func has little of bug...
+int rear_dequeue(){
+	int x=-1;
+	node *q=front_deq;
+	node *p=0;
+	while(q->next){
+		p=q;
+		q=q->next;
+	}
+	cout<<"rear_deq ele: "<<p->data<<endl;
+	x=rear_deq->data;
+	if(p){
+		p->next=0;
+		// front_deq=0;
+	}
+	rear_deq=p;
+	return x;
+}
+
+void display_dequene(){
+	cout<<"Last ele: "<<rear_deq->data<<endl;
+	node *p=front_deq;
+	if(p==0)
+		cout<<"DEqueue is empty"<<endl;
+	while(p){
+		cout<<p->data<<" ";
+		p=p->next;
+	}
+	cout<<endl;
+}
+
+// ** priority queues **
+
+
+
 int main(){
+	std_file();
 	queue q(5);
 	q.enqueue(5);
 	q.enqueue(15);
@@ -68,4 +267,43 @@ int main(){
 	q.display_quene();
 	q.dequeue();
 	q.display_quene();
+
+	circular_queue q2(5);
+	q2.enqueue(2);
+	q2.enqueue(4);
+	q2.enqueue(6);
+	q2.enqueue(8);
+	q2.enqueue(10);
+	cout<<q2.dequeue()<<endl;
+	cout<<q2.dequeue()<<endl;
+	cout<<q2.dequeue()<<endl;
+	cout<<q2.dequeue()<<endl;
+	cout<<q2.dequeue()<<endl;
+
+	q2.display_quene();
+
+	// node q3;
+	// q3.enqueue(5);
+	// q3.enqueue(10);
+	// q3.display_quene();
+	enqueue(5);
+	enqueue(10);
+	enqueue(15);
+	display_quene();
+	cout<<dequeue()<<endl;
+	display_quene();
+	cout<<"** DEqueue **"<<endl;
+	rear_enqueue(10);
+	rear_enqueue(20);
+	display_dequene();
+	front_enqueue(30);
+	front_enqueue(40);
+	display_dequene();
+
+	front_dequeue();
+	rear_dequeue();
+	rear_dequeue();
+	// rear_dequeue();
+	front_dequeue();
+	display_dequene();
 }
