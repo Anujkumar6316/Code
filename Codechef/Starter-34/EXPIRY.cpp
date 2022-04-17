7c5e93c0d38b563f1263a50d0c83dd4935f33f46
anuj kumar <anujkumar63166316@gmail.com>
2022-04-17 19:17:38 +0530

    170422

---
+++ Codechef/Starter-34/EXPIRY.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#define ll long long
+using namespace std;
+
+void _stdio(){
+	#ifndef ONLINE_JUDGE
+	freopen("/home/anuj/input.txt","r",stdin);
+	freopen("/home/anuj/output.txt","w",stdout);
+	#endif
+}
+
+void sol(){
+	ll int n,k,m;
+	cin>>n>>m>>k;
+	if(m*k>=n)
+		cout<<"YES"<<endl;
+	else
+		cout<<"NO"<<endl;
+}
+
+int main(){
+	_stdio();
+	int t=1;
+	cin>>t;
+	while(t--)
+		sol();
+}
